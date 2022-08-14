aa8360d757d753cce2cf62a5ca4edd4a713ddab1
scalette <furca7e@gmail.com>
2022-08-14 22:01:22 +0200

    feat: task solved

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,23 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
+        
+        return this.middle
     }
 
     guess() {
-
+        this.mid = Math.ceil((this.min+ this.max)/2)
+        return this.mid
     }
 
     lower() {
-
+        this.setRange(this.min, this.mid)
     }
 
     greater() {
-
+        this.setRange(this.mid, this.max)
     }
 }
 
